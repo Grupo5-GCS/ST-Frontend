8af915e20bd838e0eee3766e3dbf12a577c43da1
victor-alex159 <alexbenavente322@gmail.com>
2021-06-24 20:59:50 -0500

    Se unieron componentes y se agregaron estilos

--- src/app/Pages/home/home-routing.module.ts
+++ src/app/Pages/home/home-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormUserComponent } from './form-user/form-user.component';
+import { LoginComponent } from '../login/login.component';
 
 const routes: Routes = [
-  { path: 'register', component: FormUserComponent }
+  { path: 'fl', component: LoginComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UserRoutingModule { }
+export class HomeRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './shared/main/main.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
