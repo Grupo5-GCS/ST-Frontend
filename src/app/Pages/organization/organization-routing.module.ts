9a76cae239921d1a021f9ff0196b18434e6f535b
victor-alex159 <alexbenavente322@gmail.com>
2021-06-24 21:20:42 -0500

    Se agrego estilos y se cambio la carpeta de imagenes

--- src/app/Pages/organization/organization-routing.module.ts
+++ src/app/Pages/organization/organization-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormOrganizationComponent } from './form-organization/form-organization.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: FormOrganizationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
